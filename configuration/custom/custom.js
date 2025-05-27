913e752a3a6d793cdbc664ff0e3ee31e745627d0
GarryLowther <GarryLowther@users.noreply.github.com>
2025-05-27 15:33:48 +0000

    deploy: 465708e82c006aed91e523c3841a66ac8b0904a0

--- configuration/custom/custom.js
+++ configuration/custom/custom.js
@@ -1,8 +1,8 @@
-﻿// 27 May 2025: Custom JavaScript for flexiva.trisys.co.uk
+// 27 May 2025: Custom JavaScript for flexiva.trisys.co.uk
 //
 var FlexivaCustomDeployment =
 {
-	AppStarted: function()
+	AppStarted: function ()
 	{
 		console.log("Flexiva Custom Deployment Started");
 
@@ -12,7 +12,7 @@ var FlexivaCustomDeployment =
 		TriSysApex.Constants.ApplicationName = 'TriSys Flexiva';
 		TriSysApex.Constants.URL = 'https://flexiva.trisys.co.uk';
 		TriSysApex.Constants.WhiteLabelled = true;
-		TriSysApex.Constants.AboutImage = 'images/trisys/Generix-Portal-Splash-Screen.png';
+		TriSysApex.Constants.AboutImage = 'images/trisys/acme-Splash-Screen.png';
 
 		// TODO: Custom pre-login pages and nav bar 
 		var config = TriSysApex.Forms.Configuration;
