465708e82c006aed91e523c3841a66ac8b0904a0
Garry Lowther <development@trisys.co.uk>
2025-05-27 16:32:41 +0100

    Testing custom file override

---
+++ configuration/custom/ReadMe.txt
@@ -0,0 +1,5 @@
+27 May 2025
+-----------
+
+This custom.js file is used to override the default configuration settings of the application. 
+It allows us to customize various aspects of the application behavior and appearance.
---
+++ configuration/custom/custom.js
@@ -0,0 +1,23 @@
+// 27 May 2025: Custom JavaScript for flexiva.trisys.co.uk
+//
+var FlexivaCustomDeployment =
+{
+	AppStarted: function ()
+	{
+		console.log("Flexiva Custom Deployment Started");
+
+		// Constants are defined in many places purely for legacy reasons
+		TriSysApex.Copyright.ShortProductName = 'TriSys';
+		TriSysApex.Copyright.HomePage = 'https://www.trisys.co.uk/';
+		TriSysApex.Constants.ApplicationName = 'TriSys Flexiva';
+		TriSysApex.Constants.URL = 'https://flexiva.trisys.co.uk';
+		TriSysApex.Constants.WhiteLabelled = true;
+		TriSysApex.Constants.AboutImage = 'images/trisys/acme-Splash-Screen.png';
+
+		// TODO: Custom pre-login pages and nav bar 
+		var config = TriSysApex.Forms.Configuration;
+		//config.Pages = ...
+		//config.Views = ...
+	}
+
+};	// FlexivaCustomDeployment
